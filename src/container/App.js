5fcd5049d6d02fe3c04c621ea1a74ff063825b9e
justwiner <392110917@qq.com>
2018-06-14 00:21:24 +0800

    完成列表的实现

--- src/container/App.js
+++ src/container/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
-import {Button} from 'antd'
+import './App.scss'
 
 class App extends Component {
   render () {
-    return (
-      <Button>My App</Button>
+    const arr = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    return (  
+      <div className="container">
+        <header>
+          <font className="title">前端</font>
+          <font> ———— </font>
+          <font>on the load</font>
+        </header>
+        <section className="content">
+          {
+            arr.map((item, index) => {
+              return (
+                <p key={index}>
+                  <font>好看的英文字体,网页中css这样写</font>
+                  <font> ———— </font>
+                  <font>2018.6.13</font>
+                </p>
+              )
+            })
+          }
+        </section>
+      </div>
     )
   }
 }
